37a984cd329dbeceaeff4ccc08259ebfbc4f056a
zokirjanov2009 <zokirjanovakbarjon846@gmail.com>
2024-10-18 14:58:08 +0500

    cusers loyiha tugadi

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <div className='footer'>
+            <div className="footer-container container">
+                <p>All Right Reversed . <a href="https://www.youtube.com/@akhrorweb">Akhror Soliyev</a></p>
+            </div>
+    </div>
+  )
+}
+
+export default Footer
